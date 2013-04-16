5966887e70d54fb00cb9adaeb517e65b812c7111
Jeff Cohen <jeff@starterleague.com>
2013-04-15 19:06:19 -0500

    Add actions to the Movies controller

--- lecture_code/MoviesApp/app/controllers/movies_controller.rb
+++ lecture_code/MoviesApp/app/controllers/movies_controller.rb
@@ -1,8 +1,41 @@
 class MoviesController < ApplicationController
 
+  def create
+    #....
+  end
+  def new
+
+  end
+  def details
+    @movies = { 1 => { title: 'Apollo 13', director: 'Ron Howard', year: 2005 },
+                2 => { title: 'Star Wars', director: 'George Lucas', year: 1977 },
+                3 => { title: 'Casablanca', director: 'Me', year: 1941 },
+                 }
+
+    @movie = @movies[params[:id].to_i]
+
+    # render 'hello'
+    # render text: 'Hello'
+    # render
+  end
+
   def index
     @movies = [ { id: 1, title: 'Apollo 13', director: 'Ron Howard', year: 2005 },
                 { id: 2, title: 'Star Wars', director: 'George Lucas', year: 1977 } ]
+
+    # @title = params["title"] || "Movies"
+
+    @title = params[:title] if params[:title].present?
+    @title ||= "Movies"
+    # @title = @title || "Movies"
+
+
+    # querystring_title = params["title"]
+
+    # if querystring_title.present?
+    #   @title = querystring_title
+    # end
+
   end
 
 end
--- lecture_code/MoviesApp/config/routes.rb
+++ lecture_code/MoviesApp/config/routes.rb
@@ -2,5 +2,11 @@ MoviesApp::Application.routes.draw do
 
   get '/movies', controller: 'movies', action: 'index', as: 'movies'
 
+  get '/movies/new', controller: 'movies', action: 'new', as: 'new_movie'
+
+  get '/movies/:id', controller: 'movies', action: 'details', as: 'movie'
+
+
+  post '/movies/create', controller: 'movies', action: 'create'
 
 end
